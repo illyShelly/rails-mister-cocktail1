8487818345c6d97b8a4188a4181dc19e00f45060
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-06-08 11:01:30 +0200

    partly done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,28 @@
 class DosesController < ApplicationController
 
-    
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    raise
+    @cocktail = Cocktail.find(params[:cocktails_id])
+    @dose = Dose.new(dose_params)
+  end
+
+  def destroy
+  end
+
+  private
+
+  # Cocktail id I am looking for, other I want to specify
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
+
+# routes ?? when to update
+# link_to back it did not work
+# to dose new (put amount of alcohol, and name of ingredient ??, permit id??)
